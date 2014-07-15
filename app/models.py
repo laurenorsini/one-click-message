efbb7e53767ec7677def9f78668858950dd3d5bb
Adam <sk82712@gmail.com>
2014-07-15 00:06:57 -0600

    converted

---
+++ app/models.py
@@ -0,0 +1,10 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String, Date
+from app import db
+from datetime import datetime
+
+class Message(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    text = db.Column(db.String)
