f9f1706e3ff8ecba8881bcc1f1fe1b34b5f3a690
Lauren Orsini <lauren.rae.orsini@gmail.com>
2014-07-16 18:11:54 -0400

    Revert "Templates"

---
+++ app.py
@@ -0,0 +1,29 @@
+from flask import Flask 
+from flask import render_template
+from flask import request, redirect
+
+from twilio.rest import TwilioRestClient 
+
+app = Flask(__name__) # Creating the Flask app
+client = TwilioRestClient ('ABC', '0123') # Paste in your AccountSID and AuthToken here
+twilio_number = "+1234567890" # Replace with your Twilio number
+
+@app.route("/") # When you go to top page of app, this is what it will execute
+def main():
+    return render_template('form.html')
+  
+@app.route("/submit-form/", methods = ['POST']) 
+def submit_number():
+    number = request.form['number']
+    formatted_number = "+1" + number # Switch to your country code of choice
+    client.messages.create(to=formatted_number, from_ = twilio_number, body = "Message of your choice to text people.") # Replace body with your message of choice
+    return redirect('/messages/')
+  
+@app.route("/messages/")
+def list_messages():
+    messages = client.messages.list(to=twilio_number)
+    return render_template('messages.html', messages = messages)
+    
+    
+if __name__ == '__main__': # If we're executing this app from the command line
+    app.run("0.0.0.0", port = 3000, debug = True)
